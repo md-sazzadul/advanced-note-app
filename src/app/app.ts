e2e30ee82c87f8a5db9e7e2dadd5c883cdf53eb9
md-sazzadul <md.sazzadul.islam15@gmail.com>
2025-07-26 20:15:43 +0600

    Update and Delete a Note, Schema Options: timestamps, versionKey

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,20 +5,23 @@ const app: Application = express();
 
 app.use(express.json());
 
-const noteSchema = new Schema({
-  title: { type: String, required: true, trim: true },
-  content: { type: String, default: "" },
-  category: {
-    type: String,
-    enum: ["personal", "work", "study", "other"],
-    default: "personal",
+const noteSchema = new Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    content: { type: String, default: "" },
+    category: {
+      type: String,
+      enum: ["personal", "work", "study", "other"],
+      default: "personal",
+    },
+    pinned: { type: Boolean, default: false },
+    tags: {
+      label: { type: String, required: true },
+      color: { type: String, default: "gray" },
+    },
   },
-  pinned: { type: Boolean, default: false },
-  tags: {
-    label: { type: String, required: true },
-    color: { type: String, default: "gray" },
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Note = model("Note", noteSchema);
 
@@ -57,6 +60,29 @@ app.get("/notes/:noteId", async (req: Request, res: Response) => {
   res.status(201).json({
     success: true,
     message: "Note created successfully!",
+    note,
+  });
+});
+
+app.patch("/notes/:noteId", async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+  const updatedBody = req.body;
+  const note = await Note.findByIdAndUpdate(noteId, updatedBody, { new: true });
+
+  res.status(201).json({
+    success: true,
+    message: "Note updated successfully!",
+    note,
+  });
+});
+
+app.delete("/notes/:noteId", async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+  const note = await Note.findByIdAndDelete(noteId);
+
+  res.status(201).json({
+    success: true,
+    message: "Note deleted successfully!",
     note,
   });
 });
